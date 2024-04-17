7472dcc5902a26e6d1cafeb9cf04857dff257402
Yehor <egorkorsun7@gmail.com>
2024-04-17 19:43:29 +0200

    trip logic for driver added

--- src/main/java/com/example/autobase/controllers/TripController.java
+++ src/main/java/com/example/autobase/controllers/TripController.java
@@ -4,6 +4,7 @@ import com.example.autobase.controllers.response.Response;
 import com.example.autobase.models.*;
 import com.example.autobase.services.impl.CarServiceImpl;
 import com.example.autobase.services.impl.DriverServiceImpl;
+import com.example.autobase.services.impl.FinishedTripServiceImpl;
 import com.example.autobase.services.impl.TripServiceImpl;
 import com.example.autobase.services.interfaces.RequestService;
 import com.example.autobase.utils.Payload;
@@ -14,7 +15,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/Trips")
@@ -22,12 +25,14 @@ import java.util.Map;
 public class TripController {
 
     private final TripServiceImpl tripService;
+    private final FinishedTripServiceImpl finishedTripService;
     private final CarServiceImpl carService;
     private final DriverServiceImpl driverService;
     private final RequestService requestService;
 
-    public TripController(TripServiceImpl tripService, CarServiceImpl carService, DriverServiceImpl driverService, RequestService requestService) {
+    public TripController(TripServiceImpl tripService, FinishedTripServiceImpl finishedTripService, CarServiceImpl carService, DriverServiceImpl driverService, RequestService requestService) {
         this.tripService = tripService;
+        this.finishedTripService = finishedTripService;
         this.carService = carService;
         this.driverService = driverService;
         this.requestService = requestService;
@@ -59,5 +64,50 @@ public class TripController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new Response<>(null, HttpStatus.INTERNAL_SERVER_ERROR.value(), "Error during Trip creating"));
         }
     }
+
+    @GetMapping("/DRIVER/{name}")
+    public ResponseEntity<Object> getTripsByDriverName(@PathVariable String name) {
+        Optional<Driver> driver = driverService.findByName(name);
+        if (driver == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new Response<>(null, HttpStatus.NOT_FOUND.value(), "Driver not found"));
+        }
+
+        List<Trip> trips = tripService.findByDriver(driver);
+        return ResponseEntity.ok().body(new Response<>(trips, HttpStatus.OK.value(), "Trips found for driver: " + name));
+    }
+
+    @PostMapping("/DRIVER/{name}")
+    public ResponseEntity<Object> finishTrip(@PathVariable String name, @RequestBody Payload payload) {
+        Optional<Driver> driverOptional = driverService.findByName(name);
+        if (!driverOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new Response<>(null, HttpStatus.NOT_FOUND.value(), "Driver not found"));
+        }
+
+        Driver driver = driverOptional.get();
+
+        Trip trip = payload.getTrip();
+
+        trip.setStatus(false);
+        tripService.create(trip);
+
+        driver.setStatus(true);
+        driver.setEarnings(driver.getEarnings().add(trip.getIntermediateCost()));
+        driverService.create(driver);
+
+        Request request = trip.getRequest();
+        request.setStatus(false);
+        requestService.create(request);
+
+        tripService.delete(trip.getId());
+
+        FinishedTrip finishedTrip = new FinishedTrip(trip.getDriver(), trip.getRequest(), trip.getCar(),trip.getIntermediateCost());
+        finishedTripService.create(finishedTrip);
+
+        return ResponseEntity.ok().body(new Response<>(trip, HttpStatus.OK.value(), "Trip finished successfully"));
+    }
+
+
 
 }
